2200a543b2c75dfc119c2939e8966cbe8b407a02
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2018-06-01 15:26:01 -0300

    Commit inicial

---
+++ src/main/java/org/otojunior/sample/app/SpringBootSampleAppApplication.java
@@ -0,0 +1,12 @@
+package org.otojunior.sample.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSampleAppApplication.class, args);
+	}
+}
