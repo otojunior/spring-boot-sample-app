96e137fb845c22e9f0b49519d78c9381f723aa18
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2018-06-06 16:00:17 -0300

    Configuração de segurança envolvendo contexto do /h2-console

---
+++ README.md
@@ -0,0 +1,6 @@
+### Conexão H2 embutida
+
+Driver Class:	org.h2.Driver
+JDBC URL:		jdbc:h2:mem:testdb
+User Name:		sa
+Password:		<blank>
---
+++ src/main/java/org/otojunior/sample/app/config/SampleSecurityConfig.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package org.otojunior.sample.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Oto Soares Coelho Junior (oto.coelho-junior@serpro.gov.br)
+ *
+ */
+@Configuration
+public class SampleSecurityConfig extends WebSecurityConfigurerAdapter {
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.
+			authorizeRequests().antMatchers("/").permitAll().and().
+			authorizeRequests().antMatchers("/h2-console/**").permitAll().and().
+			csrf().disable().
+			headers().frameOptions().disable();
+	}
+}
