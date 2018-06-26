3bf6ef9051580b23c3d7b342592739bb09588e33
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2018-06-26 10:09:31 -0300

    Definido como padrão de testes RESTful, somente o REST-assured

--- src/test/java/org/otojunior/sample/app/backend/rest/EnderecoRestTest.java
+++ src/test/java/org/otojunior/sample/app/backend/rest/EnderecoRestTest.java
@@ -3,27 +3,26 @@
  */
 package org.otojunior.sample.app.backend.rest;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.equalTo;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
 import org.otojunior.sample.app.backend.entity.Endereco;
 import org.otojunior.sample.app.backend.entity.Uf;
 import org.otojunior.sample.app.backend.service.EnderecoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
 
 /**
  * @author Oto Soares Coelho Junior (oto.coelho-junior@serpro.gov.br)
@@ -32,86 +31,15 @@ import org.springframework.test.web.servlet.MockMvc;
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers=EnderecoRest.class, secure=false)
 public class EnderecoRestTest {
-	@MockBean
-	private EnderecoService enderecoService;
-
-	@Autowired
-	private MockMvc mvc;
-	
-	/**
-	 * Test method for {@link org.otojunior.sample.app.backend.rest.EnderecoRest#findById(java.lang.Long)}.
-	 * @throws Exception 
-	 */
-	@Test
-	public void testFindAllComRegistros() throws Exception {
-		given(enderecoService.findAll()).
-		willReturn(enderecos());
-		
-		mvc.perform(get("/api/endereco").accept(MediaType.APPLICATION_JSON_UTF8)).
-			andExpect(status().isOk()).
-			andExpect(jsonPath("$[0].logradouro", equalTo("Rua Teste de Unidade 1"))).
-			andExpect(jsonPath("$[0].numero", equalTo("1201"))).
-			andExpect(jsonPath("$[0].bairro", equalTo("Bairro Teste 1"))).
-			andExpect(jsonPath("$[0].cidade", equalTo("Belo Horizonte"))).
-			andExpect(jsonPath("$[0].uf", equalTo("MG"))).
-			andExpect(jsonPath("$[0].cep", equalTo("12345678"))).
-			andExpect(jsonPath("$[1].logradouro", equalTo("Rua Teste de Unidade 2"))).
-			andExpect(jsonPath("$[1].numero", equalTo("1202"))).
-			andExpect(jsonPath("$[1].bairro", equalTo("Bairro Teste 2"))).
-			andExpect(jsonPath("$[1].cidade", equalTo("Belo Horizonte"))).
-			andExpect(jsonPath("$[1].uf", equalTo("MG"))).
-			andExpect(jsonPath("$[1].cep", equalTo("22345678")));
-	}
-
-	/**
-	 * Test method for {@link org.otojunior.sample.app.backend.rest.EnderecoRest#findByCep(java.lang.String)}.
-	 * @throws Exception 
-	 */
-	@Test
-	public void testFindByCepEncontrado() throws Exception {
-		Optional<Endereco> opt = Optional.of(enderecos().get(0));
-		
-		given(enderecoService.findByCep("12345678")).
-		willReturn(opt);
-		
-		mvc.perform(get("/api/endereco?cep=12345678").accept(MediaType.APPLICATION_JSON_UTF8)).
-			andExpect(status().isOk()).
-			andExpect(jsonPath("$.logradouro", equalTo("Rua Teste de Unidade 1"))).
-			andExpect(jsonPath("$.numero", equalTo("1201"))).
-			andExpect(jsonPath("$.bairro", equalTo("Bairro Teste 1"))).
-			andExpect(jsonPath("$.cidade", equalTo("Belo Horizonte"))).
-			andExpect(jsonPath("$.uf", equalTo("MG"))).
-			andExpect(jsonPath("$.cep", equalTo("12345678")));
-	}
-
-	/**
-	 * Test method for {@link org.otojunior.sample.app.backend.rest.EnderecoRest#findAll()}.
-	 * @throws Exception 
-	 */
-	@Test
-	public void testFindById() throws Exception {
-		Optional<Endereco> opt = Optional.of(enderecos().get(1));
-		
-		given(enderecoService.findById(2L)).
-		willReturn(opt);
-		
-		mvc.perform(get("/api/endereco/2").accept(MediaType.APPLICATION_JSON_UTF8)).
-			andExpect(status().isOk()).
-			andExpect(jsonPath("$.logradouro", equalTo("Rua Teste de Unidade 2"))).
-			andExpect(jsonPath("$.numero", equalTo("1202"))).
-			andExpect(jsonPath("$.bairro", equalTo("Bairro Teste 2"))).
-			andExpect(jsonPath("$.cidade", equalTo("Belo Horizonte"))).
-			andExpect(jsonPath("$.uf", equalTo("MG"))).
-			andExpect(jsonPath("$.cep", equalTo("22345678")));
-	}
+	private static List<Endereco> enderecos;
 	
 	/**
 	 * 
-	 * @return
 	 */
-	private List<Endereco> enderecos() {
+	@BeforeClass
+	public static void beforeClass() {
 		final int N = 3; 
-		List<Endereco> enderecos = new ArrayList<>();
+		enderecos = new ArrayList<>();
 		for (int i = 1; i <= N; i++) {
 			Endereco endereco = new Endereco();
 			endereco.setAtivo(i == N ? true : false);
@@ -122,7 +50,105 @@ public class EnderecoRestTest {
 			endereco.setUf(Uf.MG);
 			endereco.setCep(i + "2345678");
 			enderecos.add(endereco);
-		}
-		return enderecos;
+		} 
+	}
+
+	@MockBean
+	private EnderecoService enderecoService;
+	
+	@Autowired
+	private MockMvc mvc;
+	
+	/**
+	 * Test method for {@link org.otojunior.sample.app.backend.rest.EnderecoRest#findAll()}.
+	 * @throws Exception 
+	 */
+	@Test
+	public void testFindAll() throws Exception {
+		BDDMockito.
+			given(enderecoService.findAll()).
+			willReturn(enderecos);
+		
+		RestAssuredMockMvc.
+			given().mockMvc(mvc).
+			when().get("/api/endereco").
+			then().
+				statusCode(200).
+				root("[1]").
+				body("logradouro", equalTo("Rua Teste de Unidade 2")).
+				body("numero", equalTo("1202")).
+				body("bairro", equalTo("Bairro Teste 2")).
+				body("cidade", equalTo("Belo Horizonte")).
+				body("uf", equalTo(Uf.MG.getSigla())).
+				body("cep", equalTo("22345678"));
+	}
+	
+	/**
+	 * Test method for {@link org.otojunior.sample.app.backend.rest.EnderecoRest#findAll()}.
+	 * @throws Exception 
+	 */
+	@Test
+	public void testFindByCep() throws Exception {
+		Optional<Endereco> opt = Optional.of(enderecos.get(1));
+		
+		BDDMockito.
+			given(enderecoService.findByCep("22345678")).
+			willReturn(opt);
+		
+		RestAssuredMockMvc.
+			given().mockMvc(mvc).
+			param("cep", "22345678").
+			when().get("/api/endereco").
+			then().
+				statusCode(200).
+				body("logradouro", equalTo("Rua Teste de Unidade 2")).
+				body("numero", equalTo("1202")).
+				body("bairro", equalTo("Bairro Teste 2")).
+				body("cidade", equalTo("Belo Horizonte")).
+				body("uf", equalTo(Uf.MG.getSigla())).
+				body("cep", equalTo("22345678"));
+	}
+	
+	/**
+	 * Test method for {@link org.otojunior.sample.app.backend.rest.EnderecoRest#findAll()}.
+	 * @throws Exception 
+	 */
+	@Test
+	public void testFindById() throws Exception {
+		Optional<Endereco> opt = Optional.of(enderecos.get(1));
+		
+		BDDMockito.
+			given(enderecoService.findById(2L)).
+			willReturn(opt);
+		
+		RestAssuredMockMvc.
+			given().mockMvc(mvc).
+			when().get("/api/endereco/2").
+			then().
+				statusCode(200).
+				body("logradouro", equalTo("Rua Teste de Unidade 2")).
+				body("numero", equalTo("1202")).
+				body("bairro", equalTo("Bairro Teste 2")).
+				body("cidade", equalTo("Belo Horizonte")).
+				body("uf", equalTo(Uf.MG.getSigla())).
+				body("cep", equalTo("22345678"));
+	}
+	
+	/**
+	 * Test method for {@link org.otojunior.sample.app.backend.rest.EnderecoRest#findAll()}.
+	 * @throws Exception 
+	 */
+	@Test
+	public void testFindByIdNaoEncontrado() throws Exception {
+		Optional<Endereco> opt = Optional.of(enderecos.get(1));
+		
+		BDDMockito.
+			given(enderecoService.findById(2L)).
+			willReturn(opt);
+		
+		RestAssuredMockMvc.
+			given().mockMvc(mvc).
+			when().get("/api/endereco/9").
+			then().statusCode(404);
 	}
 }
