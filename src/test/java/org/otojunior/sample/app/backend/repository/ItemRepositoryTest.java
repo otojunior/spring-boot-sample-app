c58083dba88d399e7eeb3d0a587effe2a9599ced
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2018-06-27 13:33:41 -0300

    Correção do teste de ItemRepository

--- src/test/java/org/otojunior/sample/app/backend/repository/ItemRepositoryTest.java
+++ src/test/java/org/otojunior/sample/app/backend/repository/ItemRepositoryTest.java
@@ -6,19 +6,12 @@ package org.otojunior.sample.app.backend.repository;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
-import org.otojunior.sample.app.backend.entity.Item;
-import org.otojunior.sample.app.backend.entity.ItemTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -29,21 +22,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class ItemRepositoryTest {
-	@Autowired private TestEntityManager entityManager;
-	@Autowired private ItemRepository repository;
-	
-	private final List<Item> MOCK_ITEMS = Arrays.asList(
-		ItemTest.of(100L, "Alicate"),
-		ItemTest.of(200L, "Martelo"),
-		ItemTest.of(300L, "Parafuso"));
-
-	/**
-	 * 
-	 */
-	@Before
-	public void before() {
-		MOCK_ITEMS.forEach(entityManager::persist);
-	}
+	@Autowired
+	private ItemRepository repository;
 	
 	/**
 	 * Test method for {@link org.otojunior.sample.app.backend.repository.ItemRepository#findByCodigo(java.lang.Long)}.
@@ -51,8 +31,8 @@ public class ItemRepositoryTest {
 	@Test
 	public void testFindByCodigo() {
 		assertEquals("Alicate", repository.findByCodigo(100L).get().getNome());
-		assertEquals("Martelo", repository.findByCodigo(200L).get().getNome());
-		assertEquals("Parafuso", repository.findByCodigo(300L).get().getNome());
+		assertEquals("Martelo", repository.findByCodigo(150L).get().getNome());
+		assertEquals("Parafuso", repository.findByCodigo(200L).get().getNome());
 	}
 	
 	/**
@@ -60,7 +40,7 @@ public class ItemRepositoryTest {
 	 */
 	@Test
 	public void testFindByCodigoInexistente() {
-		assertFalse(repository.findByCodigo(500L).isPresent());
+		assertFalse(repository.findByCodigo(Long.MAX_VALUE).isPresent());
 	}
 
 	/**
@@ -69,8 +49,8 @@ public class ItemRepositoryTest {
 	@Test
 	public void testFindByNome() {
 		assertEquals(Long.valueOf(100L), repository.findByNome("Alicate").get().getCodigo());
-		assertEquals(Long.valueOf(200L), repository.findByNome("Martelo").get().getCodigo());
-		assertEquals(Long.valueOf(300L), repository.findByNome("Parafuso").get().getCodigo());
+		assertEquals(Long.valueOf(150L), repository.findByNome("Martelo").get().getCodigo());
+		assertEquals(Long.valueOf(200L), repository.findByNome("Parafuso").get().getCodigo());
 	}
 	
 	/**
