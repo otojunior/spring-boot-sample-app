da93be1a76a2fdeaa0338ad739e2b770969c97ea
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2018-12-24 11:59:23 -0200

    Configuração de regras de segurança

--- src/main/java/org/otojunior/sample/app/config/SampleSecurityConfig.java
+++ src/main/java/org/otojunior/sample/app/config/SampleSecurityConfig.java
@@ -3,10 +3,15 @@
  */
 package org.otojunior.sample.app.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * @author Oto Soares Coelho Junior (oto.coelho-junior@serpro.gov.br)
@@ -17,16 +22,45 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SampleSecurityConfig extends WebSecurityConfigurerAdapter {
+	private final String[] PERMITED = {
+		"/actuator/**",
+		"/h2-console/**",
+		"/css/**",
+		"/imagens/**" };
+	
+	private final String[] FOR_USER_ROLE = {
+		"/item/**" };
+	
+	private final String[] FOR_ADMIN_ROLE = {
+		"/api/**" };
+		
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		/*
-		 * Por enquanto, a segurança de autenticação ficará desabilitada.
-		 */
-		http.httpBasic().disable().
+		http.authorizeRequests().
+			antMatchers(PERMITED).permitAll().
+			antMatchers(FOR_USER_ROLE).access("hasRole('USER') or hasRole('ADMIN')").
+			antMatchers(FOR_ADMIN_ROLE).hasRole("ADMIN").
+			anyRequest().authenticated().
+		and().
+			formLogin().permitAll().
+		and().
 			csrf().disable().
 			headers().frameOptions().disable();
 	}
+	
+	/**
+	 * 
+	 */
+	@Bean
+    protected UserDetailsService userDetailsService() {
+        // ensure the passwords are encoded properly
+        UserBuilder users = User.withDefaultPasswordEncoder();
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(users.username("user").password("passwd").roles("USER").build());
+        manager.createUser(users.username("admin").password("passwd").roles("USER","ADMIN").build());
+        return manager;
+    }
 }
