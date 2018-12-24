59a3996b9a5a3eec3921942dcc4d361f03c6e20e
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2018-12-24 12:09:16 -0200

    Refactoring da configuração de segurança

--- src/main/java/org/otojunior/sample/app/config/security/SampleSecurityAuthorizationConfig.java
+++ src/main/java/org/otojunior/sample/app/config/security/SampleSecurityAuthorizationConfig.java
@@ -1,17 +1,12 @@
 /**
  * 
  */
-package org.otojunior.sample.app.config;
+package org.otojunior.sample.app.config.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * @author Oto Soares Coelho Junior (oto.coelho-junior@serpro.gov.br)
@@ -21,7 +16,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
  */
 @Configuration
 @EnableWebSecurity
-public class SampleSecurityConfig extends WebSecurityConfigurerAdapter {
+public class SampleSecurityAuthorizationConfig extends WebSecurityConfigurerAdapter {
 	private final String[] PERMITED = {
 		"/actuator/**",
 		"/h2-console/**",
@@ -50,17 +45,4 @@ public class SampleSecurityConfig extends WebSecurityConfigurerAdapter {
 			csrf().disable().
 			headers().frameOptions().disable();
 	}
-	
-	/**
-	 * 
-	 */
-	@Bean
-    protected UserDetailsService userDetailsService() {
-        // ensure the passwords are encoded properly
-        UserBuilder users = User.withDefaultPasswordEncoder();
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(users.username("user").password("passwd").roles("USER").build());
-        manager.createUser(users.username("admin").password("passwd").roles("USER","ADMIN").build());
-        return manager;
-    }
 }
