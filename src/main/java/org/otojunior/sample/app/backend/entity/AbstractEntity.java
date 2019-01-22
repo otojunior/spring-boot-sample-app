47860390dbbe810b3c7059e45c213a0ce5d3e936
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2019-01-22 10:00:53 -0200

    Aplicação da licença Apache 2.0 nas classes Java

--- src/main/java/org/otojunior/sample/app/backend/entity/AbstractEntity.java
+++ src/main/java/org/otojunior/sample/app/backend/entity/AbstractEntity.java
@@ -1,5 +1,17 @@
-/**
- * 
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.otojunior.sample.app.backend.entity;
 
@@ -44,10 +56,9 @@ public abstract class AbstractEntity implements Serializable {
 	@Version
 	private Long versao;
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
+	/**
+	 * {@inheritDoc}
 	 */
-	/** {@inheritDoc} */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj) return true;
@@ -57,16 +68,17 @@ public abstract class AbstractEntity implements Serializable {
 		return Objects.equals(id, other.id);
 	}
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#hashCode()
+	/**
+	 * {@inheritDoc}
 	 */
-	/** {@inheritDoc} */
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
 
-	/** {@inheritDoc} */
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String toString() {
 		return ToStringBuilder.reflectionToString(this, SHORT_PREFIX_STYLE);
