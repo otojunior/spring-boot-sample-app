a4a5e217dcb64c5580bdd7c06b9aace5074f792f
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2020-06-25 17:02:57 -0300

    Correção de testes de unidade e IT de upload de arquivo

--- src/test/java/org/otojunior/sample/app/backend/rest/ArquivoRestIT.java
+++ src/test/java/org/otojunior/sample/app/backend/rest/ArquivoRestIT.java
@@ -3,8 +3,6 @@
  */
 package org.otojunior.sample.app.backend.rest;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -13,16 +11,13 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
-import org.otojunior.sample.app.backend.entity.Arquivo;
-import org.otojunior.sample.app.backend.service.ArquivoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author 01456231650
@@ -30,14 +25,9 @@ import org.springframework.test.web.servlet.MockMvc;
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(SpringRunner.class)
-@WebMvcTest(
-	controllers=ArquivoRest.class,
-	secure=false,
-	excludeFilters=@ComponentScan.Filter(type=FilterType.REGEX, pattern="org.otojunior.sample.app.frontend.advice.*"))
-public class ArquivoRestTest {
-	@MockBean
-	private ArquivoService service;
-	
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+public class ArquivoRestIT {
 	@Autowired
 	private MockMvc mvc;
 
@@ -46,9 +36,8 @@ public class ArquivoRestTest {
 	 * @throws Exception 
 	 */
 	@Test
+	@Transactional
 	public void testSave() throws Exception {
-		doReturn(new Arquivo()).when(service).save(any(Arquivo.class));
-		
 		MockMultipartFile file = new MockMultipartFile(
 			"file",
 			"ArquivoRestTest_testSave.txt",
